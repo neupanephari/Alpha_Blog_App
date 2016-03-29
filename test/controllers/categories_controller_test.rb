12259068724eb04300c6968e474a6374df78b9c4
neupanephari <neupanephari@gmail.com>
2016-03-29 02:32:04 +0000

    create test for restricting to admin to create categories and update the UI in nav bar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
     def setup
         @category = Category.create(name: "sports")
-end
+        @user = User.create(username: "alphatester", email: "alphatester@example.com", password:"asdf", admin: true )
+    end
     test "should get categories index" do
         get :index
         assert_response :success
     end
     test "should get new" do
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -16,6 +18,12 @@ end
     test "should get show" do
         get(:show,{'id'=> @category.id})
         assert_response :success
+    end
+    test "redirect create when admin not logged in" do
+        assert_no_difference 'Category.count' do
+            post :create, category: {name: "sports"}
+        end
+        assert_redirected_to categories_path
     end
     
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    
+    def setup
+        @user = User.create(username: "alphatester", email: "alphatester@example.com", password:"asdf", admin: true )
+    end
     test "get new category form and create category" do
+        sign_in_as(@user, "asdf")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count',1 do
@@ -12,6 +17,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     
     test "invalid category submission results in failure" do
+        sign_in_as(@user, "asdf")
          get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
